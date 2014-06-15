9d445e2851f28fd130e623d163d28558d2531f0c
jwchennlp <hit1093710417@gmail.com>
2014-06-15 12:35:03 +0800

    web server

---
+++ web_server/static/index.js
@@ -0,0 +1,35 @@
+$('form').live('submit', function(){
+    var formData = new FormData($(this)[0]);
+    $.ajax({
+        url:"/upload",
+        type:"POST",
+        data:formData,
+        contentType:false,
+        processData:false,
+        cache:false,
+        success:function(resp){
+            $('div#status').html(resp);
+        },
+        error:function(resp){
+            $('div#status').html(resp);
+        },
+        xhr:function(){
+            myXhr = $.ajaxSettings.xhr();
+            if(myXhr.upload){
+                myXhr.upload.addEventListener('progress',handleProgress,false);
+            }
+            return myXhr;
+        }
+    });
+    $('div#status').html('File is being uploaded...');
+    return false;
+});
+
+handleProgress = function(evnt){
+    if(evnt.lengthComputable){
+        var ratio = (evnt.loaded / evnt.total) * 100;
+        $('progress').attr({value:ratio})
+    }
+
+}
+
